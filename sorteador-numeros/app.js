24b963ccaf89c6afafcbded67a48ef941c1b02d2
laurafurtadoamaral <laurafurtadoamaral@gmail.com>
2024-03-30 22:36:04 -0300

    Sortear números aleatórios

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,8 +1,18 @@
 function sortear(){
-    let quantidade = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
-    alert (`Quantidade = ${quantidade}`);
-    alert (`De = ${de}`);
-    alert (`Até = ${ate}`);
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+    
+    alert (`Gerar ${quantidade} números de ${de} até ${ate}`);
+
+    let numerosSorteados = [];
+    for (let i=0; i < quantidade; i++){
+        let numero  = gerarNumeroAleatorio(de , ate);
+        numerosSorteados.push(numero);
+    }
+    alert (numerosSorteados);
+}    
+
+function gerarNumeroAleatorio (min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
