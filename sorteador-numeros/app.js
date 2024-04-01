dcdeae62b0eaec79630fdacbe82d8a9f35d2cd10
laurafurtadoamaral <laurafurtadoamaral@gmail.com>
2024-04-01 12:32:48 -0300

    Remover números repetidos

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -3,15 +3,18 @@ function sortear(){
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
     
-    alert (`Gerar ${quantidade} números de ${de} até ${ate}`);
-
     let numerosSorteados = [];
     for (let i=0; i < quantidade; i++){
-        let numero  = gerarNumeroAleatorio(de , ate);
-        numerosSorteados.push(numero);
+        let numero  = gerarNumeroAleatorio(de, ate);
+           
+        while (numerosSorteados.includes(numero)){
+            numero  = gerarNumeroAleatorio(de, ate);
+        }
+    numerosSorteados.push(numero);
     }
-    alert (numerosSorteados);
-}    
+    let resultado = (document.getElementById('resultado'));
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${numerosSorteados}</label>`;
+    }
 
 function gerarNumeroAleatorio (min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
