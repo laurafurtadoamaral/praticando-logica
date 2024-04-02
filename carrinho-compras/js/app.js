1b75fa2f1e5d0ce5ea56ef1555967b2f4ee3143d
laurafurtadoamaral <laurafurtadoamaral@gmail.com>
2024-04-02 13:47:00 -0300

    Ativar botão adicionar

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,20 @@
+let produto = document.getElementById('produto');
+let quantidade = document.getElementById('quantidade');
+let listaProdutos = document.getElementById('lista-produtos');
+let valorTotal = document.getElementById('valor-total');
+
+function adicionar(){
+    alert (produto.value);
+    alert (quantidade.value);
+
+// recuperar: produto, valor e quantidade
+
+// calcular preço por produto
+
+
+// adicionar produto no carrinho
+
+
+// atualizar preço total
+
+}
