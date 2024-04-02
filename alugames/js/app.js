650b88511eab5328f80e4b256a95017146ca94c6
laurafurtadoamaral <laurafurtadoamaral@gmail.com>
2024-04-02 11:52:15 -0300

    Ajuste botões e imagens.

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,12 +1,19 @@
-function alterarStatus(numeroJogo){
-let jogo = document.getElementById('game-' + numeroJogo);
-     if (jogo.classList.contains('dashboard__item__button')){
-        jogo.classList.remove('dashboard__item__button');
-        jogo.classList.add('dashboard__item__button--return');
-        jogo.innerHTML = '<a onclick="alterarStatus('+ numeroJogo +')" href="#" class="dashboard__item__button dashboard__item__button--return">Devolver</a>';
-    } else {
-        jogo.classList.remove('dashboard__item__button--return');
-        jogo.classList.add('dashboard__item__button');
-        jogo.innerHTML = '<a onclick="alterarStatus('+ numeroJogo +')" href="#" class="dashboard__item__button">Alugar</a>';
+function alterarStatus(numero){
+
+let jogo = document.getElementById(`game-${numero}`);
+let imagem = jogo.querySelector('.dashboard__item__img');
+let botao = jogo.querySelector('.dashboard__item__button');
+
+// se jogo alugado
+if (imagem.classList.contains('dashboard__item__img--rented')){
+    imagem.classList.remove('dashboard__item__img--rented');
+    botao.classList.remove('dashboard__item__button--return');
+    botao.textContent = 'Alugar';
+
+// deixa jogo disponível
+    }else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
     }
 }
