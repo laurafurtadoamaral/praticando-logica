c598bc20249968349be0b64f5fef87ae8534c83a
laurafurtadoamaral <laurafurtadoamaral@gmail.com>
2024-04-02 14:44:54 -0300

    Adicionar produtos ao carrinho

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,19 +1,17 @@
-let produto = document.getElementById('produto');
-let quantidade = document.getElementById('quantidade');
-let listaProdutos = document.getElementById('lista-produtos');
 let valorTotal = document.getElementById('valor-total');
 
-function adicionar(){
-    alert (produto.value);
-    alert (quantidade.value);
-
-// recuperar: produto, valor e quantidade
-
-// calcular preço por produto
-
+function adicionar(){ 
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('$')[1];
+    let quantidade = document.getElementById('quantidade').value;    
+    let preco = quantidade * valorUnitario;
 
 // adicionar produto no carrinho
-
+    let listaProdutos = document.getElementById('lista-produtos');
+    listaProdutos.innerHTML = listaProdutos.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${valorUnitario}</span>
+  </section>`;
 
 // atualizar preço total
 
