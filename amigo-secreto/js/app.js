0f0480f70322ca9ea3c2ce987fa7400406960abe
laurafurtadoamaral <laurafurtadoamaral@gmail.com>
2024-04-03 20:48:32 -0300

    Ativa função reiniciar

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,21 +1,22 @@
 let listaAmigos = [];
+let nomesIncluidos = document.getElementById('lista-amigos');
 
 function adicionar(){ 
     let amigo = document.getElementById('nome-amigo');
     listaAmigos.push(amigo.value);
     alert (listaAmigos);
     document.getElementById('nome-amigo').value = '';
+    nomesIncluidos.innerHTML = listaAmigos.join(' , ');
+
 }
-
-
-
 
 function sortear(){
 
 }
 
 
-
 function reiniciar(){
-
+    listaAmigos = [];
+    nomesIncluidos.innerHTML = '';
+    alert ('Dados zerados');
 }
