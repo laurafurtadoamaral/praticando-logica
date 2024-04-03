841258593d061d49d72a73956d5e2c6526436c9c
laurafurtadoamaral <laurafurtadoamaral@gmail.com>
2024-04-03 20:24:51 -0300

    Ativa função Adicionar

---
+++ amigo-secreto/js/app.js
@@ -0,0 +1,21 @@
+let listaAmigos = [];
+
+function adicionar(){ 
+    let amigo = document.getElementById('nome-amigo');
+    listaAmigos.push(amigo.value);
+    alert (listaAmigos);
+    document.getElementById('nome-amigo').value = '';
+}
+
+
+
+
+function sortear(){
+
+}
+
+
+
+function reiniciar(){
+
+}
