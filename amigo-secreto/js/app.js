a976ff1b54f0dfff1ca775b8b660c0ee54180554
laurafurtadoamaral <laurafurtadoamaral@gmail.com>
2024-04-04 11:40:32 -0300

    Ativa botão sortear

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,22 +1,27 @@
 let listaAmigos = [];
 let nomesIncluidos = document.getElementById('lista-amigos');
+let resultadoSorteio = document.getElementById('lista-sorteio');
+
 
 function adicionar(){ 
     let amigo = document.getElementById('nome-amigo');
     listaAmigos.push(amigo.value);
-    alert (listaAmigos);
     document.getElementById('nome-amigo').value = '';
     nomesIncluidos.innerHTML = listaAmigos.join(' , ');
-
 }
 
 function sortear(){
 
+    let listaEmbaralhada = listaAmigos.sort(function(){return 0.5 - Math.random()});
+    resultadoSorteio.innerHTML = listaEmbaralhada.join(' , ');
+
+    // pendente resultado correto. Linkar nomes entrada e saída não podendo ser o mesmo
 }
 
 
 function reiniciar(){
     listaAmigos = [];
     nomesIncluidos.innerHTML = '';
+    resultadoSorteio.innerHTML = ''; 
     alert ('Dados zerados');
 }
